ace93f80f4e40252c66173ffc9794b1a3aff3f25
jayjacobs <jay@beechplane.com>
2013-01-04 10:55:30 -0600

    Failing to get connection between R and Scala

--- src/r-mongo-scala/src/main/scala/rmongo/RMongo.scala
+++ src/r-mongo-scala/src/main/scala/rmongo/RMongo.scala
@@ -6,6 +6,7 @@ import collection.JavaConversions._
 import com.mongodb._
 import java.util.HashSet
 import java.util.Iterator
+import java.util.Arrays
 
 /**
  *
@@ -106,25 +107,14 @@ class RMongo(dbName: String, host: String, port: Int) {
     results.mkString("\n")
   }
 
-  def dbAggregate(collectionName: String, queries: String*): String = {
+  def dbAggregate(collectionName: String, queries: Array[String]): String = {
     val dbCollection = db.getCollection(collectionName)
     val queryArray = new Array[DBObject](queries.length)
     for ( i <- 0 to (queries.length - 1) ) {
         val query = queries(i)
         queryArray(i) = JSON.parse(query).asInstanceOf[DBObject]
     }
-    var aggregateIterator = dbCollection.aggregate(queryArray(0)).results.iterator
-    if (queryArray.length == 2) {
-        aggregateIterator = dbCollection.aggregate(queryArray(0), queryArray(1)).results.iterator
-    } else if (queryArray.length == 3) {
-        aggregateIterator = dbCollection.aggregate(queryArray(0), queryArray(1), queryArray(2)).results.iterator
-    } else if (queryArray.length == 4) {
-        aggregateIterator = dbCollection.aggregate(queryArray(0), queryArray(1), queryArray(2), queryArray(3)).results.iterator
-    } else if (queryArray.length == 5) {
-        aggregateIterator = dbCollection.aggregate(queryArray(0), queryArray(1), queryArray(2), queryArray(3), queryArray(4)).results.iterator
-    } else if (queryArray.length == 6) {
-        aggregateIterator = dbCollection.aggregate(queryArray(0), queryArray(1), queryArray(2), queryArray(3), queryArray(4), queryArray(5)).results.iterator
-    }
+    var aggregateIterator = dbCollection.aggregate(queryArray(0), Arrays.copyOfRange(queryArray, 1, queryArray.length):_*).results.iterator
         
     val results = ListBuffer[String]()
     while (aggregateIterator.hasNext) {
